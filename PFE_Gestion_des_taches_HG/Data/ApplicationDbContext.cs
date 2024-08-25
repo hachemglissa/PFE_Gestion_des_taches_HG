6569a6f2afdab38f958f46617c1e1cd57b5b4246
hachemglissa <hachemglissa74@gmail.com>
2024-08-25 17:30:47 +0100

    dbContext new

---
+++ PFE_Gestion_des_taches_HG/Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using PFE_Gestion_des_taches_HG.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace PFE_Gestion_des_taches_HG.Data;
+
+
+public class ApplicationDbContext : IdentityUserContext<ApplicationUser>
+{
+    
+    public DbSet<Projet> Projets => Set<Projet>();
+
+    public DbSet<Tache> Taches => Set<Tache>();
+
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Tache>()
+            .HasOne(t => t.Project)
+            .WithMany(p => p.Taches)
+            .HasForeignKey(t => t.ProjectId);
+    }
+}
