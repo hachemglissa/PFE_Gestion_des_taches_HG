e3d63a557e3f70e5e7c5d6ce52c768ccef435266
hachemglissa <hachemglissa74@gmail.com>
2024-08-25 17:37:07 +0100

    prog

---
+++ PFE_Gestion_des_taches_HG/Program.cs
@@ -0,0 +1,143 @@
+using System;
+using System.Text;
+using System.Text.Json.Serialization;
+using PFE_Gestion_des_taches_HG.Models;
+using PFE_Gestion_des_taches_HG.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using PFE_Gestion_des_taches_HG.Data;
+using PFE_Gestion_des_taches_HG.Services.Abstraction;
+using PFE_Gestion_des_taches_HG.Services.Implementation;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+// Add CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Test API", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
+
+builder.Services.AddProblemDetails();
+builder.Services.AddApiVersioning();
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+// Add DB Contexts
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer("server=LAPTOP-327JDN3K;Database=Gestion_des_taches;Trusted_Connection=True;TrustServerCertificate=True;")
+);
+
+// Register TokenService dependency
+builder.Services.AddScoped<TokenService, TokenService>();
+// Register your service with the DI container
+builder.Services.AddScoped<ITacheService, TachesService>();
+
+// Support string to enum conversions
+builder.Services.AddControllers().AddJsonOptions(opt =>
+{
+    opt.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+
+// Specify identity requirements
+builder.Services
+    .AddIdentity<ApplicationUser, IdentityRole>(options =>
+    {
+        options.SignIn.RequireConfirmedAccount = false;
+        options.User.RequireUniqueEmail = true;
+        options.Password.RequireDigit = false;
+        options.Password.RequiredLength = 6;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+    })
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>(); // Use DataContext here instead of ApplicationDbContext
+
+// Get JWT token settings from configuration
+var jwtTokenSettings = builder.Configuration.GetSection("JwtTokenSettings");
+var validIssuer = jwtTokenSettings.GetValue<string>("ValidIssuer");
+var validAudience = jwtTokenSettings.GetValue<string>("ValidAudience");
+var symmetricSecurityKey = jwtTokenSettings.GetValue<string>("SymmetricSecurityKey");
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.IncludeErrorDetails = true;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ClockSkew = TimeSpan.Zero,
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = validIssuer,
+        ValidAudience = validAudience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(symmetricSecurityKey)),
+    };
+});
+
+// Build the app
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseStatusCodePages();
+app.UseCors("AllowAll");
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
